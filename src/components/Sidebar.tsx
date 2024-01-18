dfe1163bd8ec6c82397d43bba65a2f430517afe8
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-18 15:03:20 -0700

    add text color and li

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,8 +3,16 @@ import whitelogo from '../assets/black-logo.png';
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="h-screen bg-black py-24" style={{ width: '320px' }}>
+    <div className="h-screen bg-black py-24 text-white" style={{ width: '320px' }}>
       <img src={whitelogo} alt="Logo" className="logo-class" />
+
+      <ul className="mt-12">
+        <li className="my-2">Home</li>
+        <li className="my-2">Photography</li>
+        <li className="my-2">Videography</li>
+        <li className="my-2">Social Media Management</li>
+        <li className="my-2">Contact</li>
+      </ul>
     </div>
   );
 }
