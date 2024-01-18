d8ffb47515ad1ab05f21d78fd2a9f066f5a76d98
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-18 14:31:45 -0700

    add the logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,10 @@
-const Sidebar: React.FC = () => {
-  return(
-    <div className="h-screen bg-black py-24" style={{ width: '320px' }}>
+import React from 'react';
+import whitelogo from '../assets/black-logo.png';
 
+const Sidebar: React.FC = () => {
+  return (
+    <div className="h-screen bg-black py-24" style={{ width: '320px' }}>
+      <img src={whitelogo} alt="Logo" className="logo-class" />
     </div>
   );
 }
