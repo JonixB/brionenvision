3281164e5cc4dc5e0a32a6d8b096c59293fabce1
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-18 15:26:47 -0700

    change font opacity and text padding

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,15 +3,15 @@ import whitelogo from '../assets/black-logo.png';
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="h-screen bg-black py-24 text-white" style={{ width: '320px' }}>
+    <div className="h-screen bg-black py-24 text-white text-opacity-85 font-semibold text-lg" style={{ width: '320px' }}>
       <img src={whitelogo} alt="Logo" className="logo-class" />
 
       <ul className="mt-12">
-        <li className="my-2">Home</li>
-        <li className="my-2">Photography</li>
-        <li className="my-2">Videography</li>
-        <li className="my-2">Social Media Management</li>
-        <li className="my-2">Contact</li>
+        <li className="my-2 px-4 py-2">Home</li>
+        <li className="my-2 px-4 py-2">Photography</li>
+        <li className="my-2 px-4 py-2">Videography</li>
+        <li className="my-2 px-4 py-2">Social Media Management</li>
+        <li className="my-2 px-4 py-2">Contact</li>
       </ul>
     </div>
   );
