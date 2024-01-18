a2a3e5cadc6698a2184af7f54b77f1876d5950d1
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-18 15:59:34 -0700

    import VideoBG to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import VideoBG from './components/VideoBG';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <VideoBG />
     </div>
   );
 }
