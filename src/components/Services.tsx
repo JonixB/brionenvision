0d096761dfa03ff91f7f6fca7af214a913e7479a
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-19 16:43:26 -0700

    initial styling of service cards

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import logo from '../assets/black-logo.png'
+
+const ServiceCard: React.FC<{ title: string; image: string; buttonText: string }> = ({ title, image, buttonText }) => {
+  return (
+    <div className="flex flex-col items-center p-4">
+      <h2 className="mt-4 mb-2 font-bold text-lg">{title}</h2>
+      <img src={image} alt={title} className="h-48 w-full object-cover" />
+      <button className="px-6 py-2 border border-transparent text-base font-medium rounded-md text-black bg-white hover:bg-gray-50">
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 
 const Services: React.FC = () => {
   return (
-    <div className="bg-white h-[740px]">
-      
+    <div className="bg-white h-[740px] p-8">
+      <div className="flex justify-center gap-x-12">
+        <ServiceCard title="PHOTOGRAPHY" image={logo} buttonText="VIEW PROJECTS" />
+        <ServiceCard title="VIDEOGRAPHY" image={logo} buttonText="VIEW PROJECTS" />
+        <ServiceCard title="SOCIAL MEDIA MANAGEMENT" image={logo} buttonText="FIND OUT MORE" />
+      </div>
     </div>
   );
 }
