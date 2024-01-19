8e2e3d6f298df54b5317938d7776fab72bc3beaa
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-18 17:51:45 -0700

    add videos

--- src/components/VideoBG.tsx
+++ src/components/VideoBG.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Video from '../assets/Engagement.m4v';
 
 const VideoBG: React.FC = () => {
   return (
-    <div className="relative">
-      <video 
-        autoPlay 
-        loop 
-        muted 
-        className="w-full" 
-        src="path_to_your_video.mp4"
+    <div className="relative h-screen w-full overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src={Video}
       >
         Your browser does not support the video tag.
       </video>
@@ -16,7 +17,7 @@ const VideoBG: React.FC = () => {
       <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         Projects
       </button>
-    </div>
+    </div >
   );
 }
 
