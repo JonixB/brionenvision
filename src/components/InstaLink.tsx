6e7c5066e8c3d539e55f3abb530a88525da9b8c6
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-28 13:19:35 -0700

    increase duration of animation

--- src/components/InstaLink.tsx
+++ src/components/InstaLink.tsx
@@ -10,7 +10,7 @@ const InstaLink: React.FC<InstaLinkProps> = ({ imageUrl, reelUrl }) => {
   return (
     <a href={reelUrl} target="_blank" rel="noopener noreferrer" className="relative group">
       <img src={imageUrl} alt="Instagram Reel" className="transition duration-300 ease-in-out group-hover:opacity-50" style={{ width: '272px', height: '272px' }} />
-      <FiInstagram className="absolute bottom-2 right-2 w-8 h-8 text-white opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out" />
+      <FiInstagram className="absolute bottom-2 right-2 w-8 h-8 text-white opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out" />
     </a>
   );
 };
