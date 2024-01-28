9a9cd1aa526d918e8fe24fe4a80474684bb40ce7
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-28 13:17:29 -0700

    add animation to darken image and display instagram icon on hover

--- src/components/InstaLink.tsx
+++ src/components/InstaLink.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiInstagram } from 'react-icons/fi';
 
 type InstaLinkProps = {
   imageUrl: string;
@@ -7,8 +8,9 @@ type InstaLinkProps = {
 
 const InstaLink: React.FC<InstaLinkProps> = ({ imageUrl, reelUrl }) => {
   return (
-    <a href={reelUrl} target="_blank" rel="noopener noreferrer">
-      <img src={imageUrl} alt="Instagram Reel" style={{ width: '272px', height: '272px' }} />
+    <a href={reelUrl} target="_blank" rel="noopener noreferrer" className="relative group">
+      <img src={imageUrl} alt="Instagram Reel" className="transition duration-300 ease-in-out group-hover:opacity-50" style={{ width: '272px', height: '272px' }} />
+      <FiInstagram className="absolute bottom-2 right-2 w-8 h-8 text-white opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out" />
     </a>
   );
 };
