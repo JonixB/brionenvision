900f2705ea2bbd18db2b3b133083611050def5e4
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-29 20:50:39 -0700

    setup the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import GrpHome from './components/GrpHome';
+import GrpRealEstate from './components/GrpRealEstate';
 
 function App() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex-grow overflow-auto">
-        <GrpHome />
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex-grow overflow-auto">
+          <Routes>
+            <Route path="/" element={<GrpHome />} />
+            <Route path="/real-estate" element={<GrpRealEstate />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
