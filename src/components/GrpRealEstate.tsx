5b48aa39a62e4f159a20b84d0c0a332e82d9c418
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-29 20:43:42 -0700

    Group RealEstate components into this component

---
+++ src/components/GrpRealEstate.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RealEstate from './RealEstate';
+import ContactUs from './ContactUs';
+
+const GrpRealEstate: React.FC = () => {
+  return (
+    <div>
+      <RealEstate />
+      <ContactUs />
+    </div>
+  );
+};
+
+export default GrpRealEstate;
